3ecf953ffe5327a8b1dbcf0a463e9ebddcadb7e3
Casey Holloway <cholloway@softerware.com>
2022-12-15 09:12:06 -0500

    import re,  create empty totals list, refactor for loop.

--- src/Day1/Part1.py
+++ src/Day1/Part1.py
@@ -1,15 +1,15 @@
-elves_snacks_calories = open('C:\Projects\Git\AoC-2022\src\Day1\Day1Input.txt', 'r')
+import re
+elves_snacks_calories = open('C:\Projects\Git\AoC-2022\src\Day1\Day1Input.txt', 'r').readlines()
 
-most_calories = 0
-elf_snacks_total_calories = 0
+elf_total_calories = 0
+totals = []
 
 for calories_in_a_snack in elves_snacks_calories:    
-    if calories_in_a_snack != '\n':
+    if  re.findall('[0-9]', calories_in_a_snack):
         calories_in_a_snack = int(calories_in_a_snack.replace('\n', ''))
-        elf_snacks_total_calories += calories_in_a_snack
+        elf_total_calories += calories_in_a_snack               
     else:
-        if elf_snacks_total_calories > most_calories:
-            most_calories = elf_snacks_total_calories   
-        elf_snacks_total_calories = 0
-print(f'The Elf with the most calories is carrying **{str(most_calories)}** calories.')
-    
+        totals.append(elf_total_calories)
+        elf_total_calories = 0
+totals.append(elf_total_calories)
+print(f'The Elf with the most calories is carrying **{str(max(totals))}** calories.')
